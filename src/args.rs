d01866e720f054f7298de46da1d514469cc32829
Sherif Hussien <sherifhussien1996@gmail.com>
2024-11-03 00:16:07 +0100

    feat: parse cli args

---
+++ src/args.rs
@@ -0,0 +1,21 @@
+use clap::{Arg, Command, crate_version};
+
+
+pub fn parse_args() -> i32 {
+    let matches = Command::new("ht")
+        .version(crate_version!())
+        .about("A hash table that supports concurrent operations")
+        .arg(
+            Arg::new("hash_table_size")
+                .short('n')
+                .long("ht_size")
+                .default_value("10")
+                .help("Set the hash table size")
+        )
+        .get_matches(); // parse env::args_os
+
+    let ht_size = matches.get_one::<String>("hash_table_size").unwrap();
+    let ht_size: i32 = ht_size.parse().expect("provided hash table size is not a number!");
+
+    ht_size
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,6 @@
+mod args;
+
 fn main() {
+    let ht_size: i32 = args::parse_args();
+    println!("Hash table size: {ht_size}");
 }

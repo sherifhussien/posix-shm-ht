c56dd2f15ef2b2484544d9c9823f8f168c885fe1
Sherif Hussien <sherifhussien1996@gmail.com>
2024-11-06 01:18:13 +0100

    feat: implement ht data structure

--- server/src/hash_table.rs
+++ server/src/hash_table.rs
@@ -1,5 +1,7 @@
 use std::collections::LinkedList;
 use std::hash::{DefaultHasher, Hash, Hasher};
+use std::borrow::Borrow;
+use std::{mem, usize};
 
 #[derive(Debug)]
 pub struct HashTable<K, V> {
@@ -11,7 +13,7 @@ pub struct HashTable<K, V> {
 
 impl<K, V> HashTable<K, V>
 where
-    K: Hash + Eq + Clone,
+    K: Hash + Eq + Clone + std::fmt::Debug,
     V: Clone,
 {
 
@@ -23,11 +25,77 @@ where
     }
 
     /// returns an index
-    pub fn hash(&self, key: &K) -> usize {
+    pub fn hash<Q>(&self, key: &Q) -> usize 
+    where
+    K: Borrow<Q>,
+    Q: Hash + ?Sized
+    {
         let mut s = DefaultHasher::new();
         key.hash(&mut s);
         
         s.finish() as usize % self.buckets.len()
+    }
+
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        let idx: usize = self.hash(&k);
+        let bucket: &mut LinkedList<(K, V)> = &mut self.buckets[idx];
+
+        for (old_key, old_value) in bucket.iter_mut() {
+            if k == *old_key {
+                return Some(mem::replace(old_value, v));
+            }
+        }
+
+        // hash table did not have this key present
+        bucket.push_back((k, v));
+        self.size += 1;
+        return None
+    }
+
+    pub fn get<Q>(&self, k: &Q) -> Option<&V>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let index: usize = self.hash(k);
+        let bucket: &LinkedList<(K, V)> = &self.buckets[index];
+
+        for (key, value) in bucket.iter() {
+            if k == key.borrow() {
+                return Some(value);
+            }
+        }
+        // hash table did not have this key present
+        None
+    }
+
+    pub fn remove<Q>(&mut self, k: &Q) -> Option<V> 
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let index: usize = self.hash(k);
+        let bucket: &mut LinkedList<(K, V)> = &mut self.buckets[index];
+
+        let mut idx = -1;
+        for (key, _) in bucket.iter() {
+            idx += 1;
+            if k == key.borrow() {
+                break;        
+            }
+        }
+
+        // workaround 
+        if idx != -1 {
+            let mut split: LinkedList<(K, V)> = bucket.split_off(idx as usize);
+            if let Some((_, value)) = split.pop_front() {
+                bucket.append(&mut split);
+                return Some(value);
+            }
+        }
+        
+        // hash table did not have this key present
+        None
     }
 
     pub fn len(&self) -> usize {
--- server/src/main.rs
+++ server/src/main.rs
@@ -5,11 +5,40 @@ use hash_table::HashTable;
 
 fn main() {
     let ht_size: i32 = args::parse_args();
-    println!("Hash table size: {ht_size}");
+    println!(">> Hash table size: {ht_size}");
 
-    let ht: HashTable<String, String> = hash_table::HashTable::new(ht_size as usize);
+    let mut ht: HashTable<String, String> = hash_table::HashTable::new(ht_size as usize);
     println!("{:?}", ht);
 
-    let idx = ht.hash(&"test2".to_string());
-    print!("{}", idx);
+
+    ht.insert("test1".to_string(), "value1".to_string());
+    println!("{:?}", ht);
+
+    ht.insert("test2".to_string(), "value2".to_string());
+    println!("{:?}", ht);
+
+    ht.insert("test3".to_string(), "value3".to_string());
+    println!("{:?}", ht);
+
+    let g1 = ht.get("test1");
+    println!("{:?}", g1);
+
+    ht.insert("test1".to_string(), "value1 updated".to_string());
+    println!("{:?}", ht);
+
+    let g1 = ht.get("test1");
+    println!("{:?}", g1);
+
+    let g1 = ht.remove("test4");
+    println!("{:?}", g1);
+
+    let g1 = ht.remove("test1");
+    println!("{:?}", g1);
+
+    ht.insert("test6".to_string(), "value6".to_string());
+    println!("{:?}", ht);
+
+    ht.insert("test7".to_string(), "value7".to_string());
+    println!("{:?}", ht);
+
 }
